9b1ffc8cdbf17c0e55bb25090bba34498a008a79
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-06-23 13:56:05 -0400

    Installs bootstrap and react-bootstrap; builds some test components

---
+++ src/components/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ClickableIcon from "./ClickableIcon"
+
+ const MainPage = () => {
+
+   return(
+     <div>
+      <header>Welcome to my website.</header>
+      <ClickableIcon/>
+     </div>
+    )
+ }
+
+export default MainPage;
