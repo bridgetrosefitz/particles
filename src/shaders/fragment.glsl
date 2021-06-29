8ab136bb51b384256348b336dd973befbd275f78
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-06-29 16:35:46 -0400

    Makes those li'l tiles into circle boys

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,6 +5,17 @@ varying vec2 vPUv;
 
 void main () {
   vec4 color = texture2D(uTexture, vPUv);
+
+  // circle
+
+  float border = 0.3;
+  float radius = 0.5;
+  float dist = radius - distance(vUv, vec2(0.5));
+  float t = smoothstep(0.0, border, dist);
+
+  // final color
+  color.a = t;
+
   gl_FragColor = color;
 
 }
